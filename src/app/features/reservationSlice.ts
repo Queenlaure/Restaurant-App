60882c3f6c235b2245b3bb3d5d957cb0e077117a
Nfongeh Queen <queennfongeh@gmail.com>
2022-08-23 10:05:29 -0400

    first commit

---
+++ src/app/features/reservationSlice.ts
@@ -0,0 +1,24 @@
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+
+interface ReservationState {
+    value: string[]
+}
+
+const initialState: ReservationState = {
+    value: []
+}
+
+export const reservationsSlice = createSlice({
+    name: 'reservation',
+    initialState,
+    reducers: {
+        addReservation: (state, action: PayloadAction<string>) => {
+            state.value.push(action.payload)
+        }
+    }
+
+})
+
+export const {addReservation} = reservationsSlice.actions;
+
+export default reservationsSlice.reducer;
